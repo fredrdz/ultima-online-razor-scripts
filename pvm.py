e67dd7503158aaf8fa6ffe2b4377423321ebd3f7
Fredy Rodriguez <fredy@thedesque.com>
2024-07-10 05:50:10 -0500

    update: pvm; refactored to prevent fizzle issue and faster daemon renaming

--- pvm.py
+++ pvm.py
@@ -30,12 +30,13 @@ pvmFilter.Paralized = -1
 pvmFilter.Notorieties = List[Byte](bytes([4, 5, 6]))
 
 # check if _defense.py is running
-if not Misc.ScriptStatus("_defense.py"):
+if Misc.ScriptStatus("_defense.py") is False:
     Misc.ScriptRun("_defense.py")
 
 while not Player.IsGhost:
     Misc.Pause(100)
 
+    # toggle war mode off if trying to do something peaceful
     if Journal.SearchByType("You are preoccupied with thoughts of a battle.", "System"):
         Player.SetWarMode(True)
         Player.SetWarMode(False)
@@ -55,49 +56,62 @@ while not Player.IsGhost:
         Misc.SetSharedValue("cast_cd", Timer.Remaining("cast_cd"))
         continue
 
-    # check if we can summon a daemon
+    # check follower count
     follower_diff = Player.FollowersMax - Player.Followers
+
+    # check if we can summon a daemon
     if 0 < follower_diff >= 2 and Player.Mana >= 50:
+        # stop casting script if running to avoid fizzles
+        if Misc.ScriptStatus("_cast.py") is True:
+            Misc.ScriptStop("_cast.py")
+            Misc.SetSharedValue("spell", "")
+            # read global cast cd timer
+            if Misc.ReadSharedValue("cast_cd") > 0:
+                current = Misc.ReadSharedValue("cast_cd")
+                Timer.Create("cast_cd", current)
+            # wait out timer
+            remaining = Timer.Remaining("cast_cd")
+            Misc.Pause(remaining)
         Spells.CastMagery("Summon Daemon")
         Timer.Create("cast_cd", spells["Summon Daemon"].delayInMs + config.shardLatency)
-        Misc.SetSharedValue("cast_cd", Timer.Remaining("cast_cd"))
+        current = Timer.Remaining("cast_cd")
+        Misc.SetSharedValue("cast_cd", current)
 
     # get enemies
     enemies = Mobiles.ApplyFilter(pvmFilter)
-    # heal if needed and then filter out enemies named "bob"
+
+    # check enemies
     for i in range(len(enemies) - 1, -1, -1):
-        if enemies[i].Name == "bob":
-            if enemies[i].Hits < enemies[i].HitsMax:
-                if (
-                    Timer.Check("bandage_cd") is False
-                    and Player.InRangeMobile(enemies[i], 2) is True
-                ):
-                    bandages = FindBandage(Player.Backpack)
-                    if bandages:
-                        if Target.HasTarget():
-                            Target.Cancel()
-                        Items.UseItem(bandages)
-                        Target.WaitForTarget(1000, False)
-                        Target.TargetExecute(enemies[i])
-                        Timer.Create("bandage_cd", 2300 + config.shardLatency)
+        e = enemies[i]
+        if e.Name == "bob":
             del enemies[i]
-    # check if enemies found and select one
-    if len(enemies) > 0:
-        for e in enemies:
-            # exception: we name our friendly followers "bob"
-            # if our daemon summon, rename, set to guard, and go to next target
-            if e.MobileID == daemon and e.Name != "bob":
-                Misc.PetRename(e, "bob")
-                Player.ChatSay(colors["debug"], "All Guard Me")
-                continue
-            # overwrites enemy mob target via script shared value if one is set
-            shared_target = Misc.ReadSharedValue("kill_target")
-            if shared_target > 0:
-                enemy = Mobiles.FindBySerial(shared_target)
-                if enemy:
-                    break
-            # select nearest enemy if shared target not found
-            enemy = Mobiles.Select(enemies, "Nearest")
+            # if enemies[i].Hits < enemies[i].HitsMax:
+            #     if (
+            #         Timer.Check("bandage_cd") is False
+            #         and Player.InRangeMobile(enemies[i], 2) is True
+            #     ):
+            #         bandages = FindBandage(Player.Backpack)
+            #         if bandages:
+            #             if Target.HasTarget():
+            #                 Target.Cancel()
+            #             Items.UseItem(bandages)
+            #             Target.WaitForTarget(200, False)
+            #             Target.TargetExecute(enemies[i])
+            #             Timer.Create("bandage_cd", 2350 + config.shardLatency)
+        elif e.MobileID == daemon and e.Name != "bob":
+            # if our daemon, rename, set to guard
+            Misc.PetRename(e, "bob")
+            Player.ChatSay(colors["debug"], "All Guard Me")
+
+    # select nearest enemy
+    enemy = Mobiles.Select(enemies, "Nearest")
+
+    # overwrite with shared target if found
+    shared_target = Misc.ReadSharedValue("kill_target")
+    if shared_target > 0:
+        found = Mobiles.FindBySerial(shared_target)
+        if found:
+            enemy = found
 
     # if enemy is found, select and kill
     if enemy and Timer.Check("kill_cd") is False:
@@ -109,9 +123,9 @@ while not Player.IsGhost:
         if Target.HasTarget():
             Target.Cancel()
         Player.ChatSay(colors["debug"], "All Kill")
-        Target.WaitForTarget(1000, False)
+        Target.WaitForTarget(200, False)
         Target.TargetExecute(enemy)
-        Timer.Create("kill_cd", 4000)
+        Timer.Create("kill_cd", 5000)
 
     # if no enemy found, clear target
     enemy = None
